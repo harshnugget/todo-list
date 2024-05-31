1306ee042a7c4e77607ea39611f63e8b2652c5a6
harshnugget <76251920+harshnugget@users.noreply.github.com>
2024-05-31 09:23:46 +0100

    Project Manager class for additional functionality

--- src/index.js
+++ src/index.js
@@ -1,28 +1,7 @@
-import { Manager, Task, Project } from './todo.js';
+import { Manager, Task, Project, ProjectManager } from './todo.js';
 import './dom.js';
 
-// Create projects and tasks
-window.project1 = new Project("Project 1");
-window.project2 = new Project("Project 2");
-
-const task1 = new Task("Task 1", "Description 1", new Date(), "High");
-const task2 = new Task("Task 2", "Description 2", new Date(), "Low");
-const task3 = new Task("Task 3", "Description 2", new Date(), "High");
-const task4 = new Task("Task 4", "Description 2", new Date(), "Low");
-
-project1.taskManager.createItem(task1);
-project1.taskManager.createItem(task2);
-project1.taskManager.createItem(task3);
-project1.taskManager.createItem(task4);
-
-const task5 = new Task("Task 5", "Description 1", new Date(), "Low");
-const task6 = new Task("Task 6", "Description 2", new Date(), "Low");
-const task7 = new Task("Task 7", "Description 2", new Date(), "High");
-const task8 = new Task("Task 8", "Description 2", new Date(), "High");
-
-project2.taskManager.createItem(task5);
-project2.taskManager.createItem(task6);
-project2.taskManager.createItem(task7);
-project2.taskManager.createItem(task8);
-
-window.projectManager = new Manager([project1, project2]);
+window.Manager = Manager;
+window.Task = Task;
+window.Project = Project;
+window.ProjectManager = ProjectManager;
--- src/todo.js
+++ src/todo.js
@@ -4,7 +4,7 @@
 // Create a class that deals with managing Tasks and Projects
 // Functionality includes creating or deleting new tasks/projects, and assigning IDs
 class Manager {
-    constructor(array = []) {
+    constructor(array=[]) {
         this.items = [];
         array.forEach(item => this.createItem(item));
     }
@@ -12,11 +12,14 @@ class Manager {
     createItem(item) {
         item.id = (this.items).length > 0 ? this.items[(this.items).length - 1].id + 1 : 0;  // Assign a unique id to each item before pushing to the array
         this.items.push(item);
+        return item;
     }
 
     deleteItem(itemId) {
-        const index = (this.items).findIndex(item => itemId === item.id); // Retrieve the index of where item id is found
-        if (index !== -1) (this.items).splice(index, 1);
+        const index = (this.items).findIndex(item => itemId == item.id); // Retrieve the index of where item id is found
+        if (index !== -1) {
+            this.items.splice(index, 1);
+        }
     }
 }
 
@@ -28,19 +31,13 @@ class Task {
         this.priority = priority.toLowerCase();
         this.status = status;
         
-        // Validate priority
-        if (this.priority !== "low" && this.priority !== "high") {
+        if (!["low", "high"].includes(this.priority)) {
             throw new Error("Priority must be either 'Low' or 'High'");
         }
 
-        // Validate status
         if (typeof this.status !== "boolean") {
-            throw new Error("Status must be either 'true' or 'false'");
+            throw new Error("Status must be a boolean");
         }
-    }
-    
-    statusToggle() {
-        this.status = this.status === false ? true : false;
     }
 }
 
@@ -51,4 +48,29 @@ class Project {
     }
 }
 
-export { Manager, Task, Project };
+class ProjectManager extends Manager {
+    constructor(project) {
+        super(project);
+        this._activeProjectId = null
+    }
+
+    set activeProject(id) {
+        if (typeof id === "number" || typeof id === "string") {
+            this._activeProjectId = id;
+        } else {
+            throw new TypeError("Setting the active project requires the 'id' value to be either a number or a string.");
+        }
+    }
+
+    get activeProject() {
+        return this.items.find(project => project.id === this._activeProjectId) || null;
+    }
+
+    createItem(project) {
+        project = super.createItem(project);
+        this._activeProjectId = project.id;
+        return project;
+    }
+}
+
+export { Manager, Task, Project, ProjectManager };
